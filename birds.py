b6bd9eef055f3690f35714c3ece2371a91b3127b
Rachel Scott <Rl5cott@outlook.com>
2023-02-17 15:50:26 -0500

    knowledge check

---
+++ birds.py
@@ -0,0 +1,27 @@
+print('hello world')
+
+
+birds = [
+    {
+    "weight" : 2.4,
+    "length" : 10,
+    "species" : "Northern Cardinal",
+    "longitude and latitude last sighted" : (38.2527, 85.7585),
+    "state bird?" : True},
+    {
+    "weight" : 1,
+    "length" : 5,
+    "species" : "Sparrow",
+    "longitude and latitude last sighted" : (38.2132, 85.7827),
+    "state bird?" : False
+    }
+]
+{'length': 10,
+ 'longitude and latitude last sighted': (38.2527, 85.7585),
+ 'species': 'Northern Cardinal',
+ 'state bird?': True,
+ 'weight': 2.4}
+
+print(type(birds[0]))
+
+birds[0]['longitude and latitude last sighted'] 
